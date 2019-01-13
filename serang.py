f99af5ee84f0779b73a2f7e6731969d4cf738501
linuxkukeren <46648684+linuxkukeren@users.noreply.github.com>
2019-01-13 22:49:30 +0800

    Update serang.py

--- serang.py
+++ serang.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-#ganti xxxxxxxxxx ( Dengan No Target )
-# Chandra Aditya
+
+
 
 import requests,json,time,subprocess
 import os, time
@@ -10,11 +9,10 @@ import subprocess as sp
 subprocess.call("clear", shell=True)
 
 banner = """
-╔╗╴╴╴╔══════╗ \033[1;34mCalling SPAM\033[1;36m UNLIMITED\033[0m
-║║╴╴╴║╴╔════╝
-║╚═══╝╴╚════╗ MoDify BY \033[1;32mAditya01\033[0m
-╚════╗╴╔═══╗║ 404 AQUA (Not FOUND)
-╔════╝╴║╴╴╴║║ SPECIAL HARI CoLY Indonesia
+╔╗╴╴╴╔══════╗ 
+║╚═══╝╴╚════╗ 
+╚════╗╴╔═══╗║ 
+╔════╝╴║╴╴╴║║ 
 ╚══════╝╴╴╴╚╝
 """
 
@@ -24,7 +22,7 @@ a = raw_input("[+] Lanjutkan (y/n): ")
 d = raw_input("[+] Jumlah : ")
 while x < d:
    b = {"https://xxnx.com":a}
-   c = " https://0x.nakocoders.org/rest-api/lain-nya/api.php?nomor=xxxxxxxxxx"
+   c = " https://0x.nakocoders.org/rest-api/lain-nya/api.php?nomor=6281344772900"
    e = requests.post(c, data=b)
    f = json.loads(e.text)
    if "nexmo_request_id" in f:
